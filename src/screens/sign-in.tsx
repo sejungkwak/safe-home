29975f045ebed2caf09e773bc181b81e968c030a
sejungkwak <kwak.sejung@gmail.com>
2026-04-14 17:47:55 +0100

    feat: add radio buttons for user role to sign in screen

--- src/screens/sign-in.tsx
+++ src/screens/sign-in.tsx
@@ -1,9 +1,9 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Link } from "expo-router";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Controller, useForm } from "react-hook-form";
 import { Alert, View } from "react-native";
-import { HelperText, Text, useTheme } from "react-native-paper";
+import { HelperText, RadioButton, Text, useTheme } from "react-native-paper";
 
 import GoogleSignIn from "@/components/auth/google-sign-in";
 import HorizontalLine from "@/components/ui/horizontal-line";
@@ -19,21 +19,42 @@ import { signinData, signinSchema } from "@/schemas/sign-in";
  */
 function SigninScreen() {
   const { colors } = useTheme();
+  const [userType, setUserType] = useState("rider");
 
   // initialise React Hook Form with Zod schema validation
   const {
     handleSubmit,
     control,
+    clearErrors,
     formState: { errors },
   } = useForm<signinData>({
     resolver: zodResolver(signinSchema),
     defaultValues: { email: "", password: "" },
   });
 
+  // clear error messages on user type changes.
+  useEffect(() => {
+    clearErrors();
+  }, [userType, clearErrors]);
+
   /**
    * Submits the user input data after validation has passed.
    */
   async function onSubmit(values: signinData) {
+    // retrieve the user role from database for the entered email
+    const { data: userProfile, error: userTypeError } = await supabase
+      .from("profile")
+      .select("role, email")
+      .eq("email", values.email)
+      .contains("role", [userType])
+      .single();
+
+    if (!userProfile || userTypeError) {
+      Alert.alert("The information you entered does not match our records.");
+      await supabase.auth.signOut();
+      return;
+    }
+
     // pass the validated email and password to Supabase Auth
     const { error } = await supabase.auth.signInWithPassword({
       email: values.email,
@@ -47,55 +68,81 @@ function SigninScreen() {
 
   return (
     <ScreenContainer>
-      <View>
-        <Controller
-          control={control}
-          name="email"
-          render={({ field: { onChange, onBlur, value } }) => (
-            <View>
-              <InputField
-                label="Email Address"
-                mode="outlined"
-                keyboardType="email-address"
-                autoCapitalize="none"
-                onChangeText={onChange}
-                onBlur={onBlur}
-                value={value}
-                error={!!errors.email}
-              />
-              <HelperText type="error" visible={!!errors.email} padding="none">
-                {errors.email?.message}
-              </HelperText>
-            </View>
-          )}
-        />
-        <Controller
-          name="password"
-          control={control}
-          render={({ field: { onChange, onBlur, value } }) => (
-            <View>
-              <InputField
-                label="Password"
-                mode="outlined"
-                autoCapitalize="none"
-                secureTextEntry
-                onChangeText={onChange}
-                onBlur={onBlur}
-                value={value}
-                error={!!errors.password}
-              />
-              <HelperText
-                type="error"
-                visible={!!errors.password}
-                padding="none"
-              >
-                {errors.password?.message}
-              </HelperText>
-            </View>
-          )}
-        />
+      <View className="mb-4">
+        <RadioButton.Group
+          onValueChange={(value) => setUserType(value)}
+          value={userType}
+        >
+          <View className="flex-row">
+            <RadioButton.Item
+              label="Rider"
+              value="rider"
+              mode="android"
+              position="leading"
+            />
+            <RadioButton.Item
+              label="Driver"
+              value="driver"
+              mode="android"
+              position="leading"
+            />
+          </View>
+        </RadioButton.Group>
+        <View>
+          <Controller
+            control={control}
+            name="email"
+            render={({ field: { onChange, onBlur, value } }) => (
+              <View>
+                <InputField
+                  label="Email Address"
+                  mode="outlined"
+                  keyboardType="email-address"
+                  autoCapitalize="none"
+                  onChangeText={onChange}
+                  onBlur={onBlur}
+                  value={value}
+                  error={!!errors.email}
+                />
+                <HelperText
+                  type="error"
+                  visible={!!errors.email}
+                  padding="none"
+                >
+                  {errors.email?.message}
+                </HelperText>
+              </View>
+            )}
+          />
+          <Controller
+            name="password"
+            control={control}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <View>
+                <InputField
+                  label="Password"
+                  mode="outlined"
+                  autoCapitalize="none"
+                  secureTextEntry
+                  onChangeText={onChange}
+                  onBlur={onBlur}
+                  value={value}
+                  error={!!errors.password}
+                />
+                <HelperText
+                  type="error"
+                  visible={!!errors.password}
+                  padding="none"
+                >
+                  {errors.password?.message}
+                </HelperText>
+              </View>
+            )}
+          />
+        </View>
+        <PrimaryButton onPress={handleSubmit(onSubmit)}>Sign In</PrimaryButton>
       </View>
-      <PrimaryButton onPress={handleSubmit(onSubmit)}>Sign In</PrimaryButton>
+
       <View className="flex-row items-center gap-2 my-4">
         <HorizontalLine />
         <Text>or</Text>
