93023a0f551158dc024bb0bb5bc5ef5476c4723a
sejungkwak <kwak.sejung@gmail.com>
2026-04-14 23:43:11 +0100

    fix: add code to get React context after app reload

--- src/context/role.tsx
+++ src/context/role.tsx
@@ -1,6 +1,8 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import React, {
   createContext,
   useContext,
+  useEffect,
   useState,
   type PropsWithChildren,
 } from "react";
@@ -8,6 +10,7 @@ import React, {
 type RoleContextType = {
   role: string | null;
   setRole: (role: string) => void;
+  isLoading: boolean;
 };
 
 // create context object
@@ -30,8 +33,37 @@ export function useRole() {
 export function RoleProvider({ children }: PropsWithChildren) {
   const [role, setRole] = useState<string | null>(null);
 
+  // persistent data storage logic adapted from
+  // https://dev.to/cathylai/managing-global-settings-in-react-native-a-clean-context-api-approach-4b20
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const loadRole = async () => {
+      try {
+        const savedRole = await AsyncStorage.getItem("user_role");
+        if (savedRole) setRole(savedRole);
+      } catch (error) {
+        throw error;
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    loadRole();
+  }, []);
+
+  const updateRole = async (newRole: string | null) => {
+    setRole(newRole);
+
+    if (newRole) {
+      await AsyncStorage.setItem("user_role", newRole);
+    } else {
+      await AsyncStorage.removeItem("user_role");
+    }
+  };
+
   return (
-    <RoleContext.Provider value={{ role, setRole }}>
+    <RoleContext.Provider value={{ role, setRole: updateRole, isLoading }}>
       {children}
     </RoleContext.Provider>
   );
