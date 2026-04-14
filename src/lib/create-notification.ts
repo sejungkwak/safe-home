c7ff8bcd320bcd55c7d0e46aafe9aa80818e3850
sejungkwak <kwak.sejung@gmail.com>
2026-04-14 14:28:41 +0100

    feat: create Supabase edge function for notification

--- src/lib/create-notification.ts
+++ src/lib/create-notification.ts
@@ -7,23 +7,57 @@ import { supabase } from "./supabase";
  * Creates new data entry for the notification table
  * which triggers Supabase webhook for sending notification.
  */
-export default async function createNotification({userId, origin, destination, dateTime, notificationType, tripId}: TripParams & {notificationType: string, tripId: string}) {
-    
-    if (!userId || !origin || !destination || !dateTime || !notificationType || !tripId) return;
+export default async function createNotification({
+  userId,
+  origin,
+  destination,
+  dateTime,
+  notificationType,
+  tripId,
+}: TripParams & { notificationType: string; tripId: string }) {
+  // return if an argument value is null
+  if (
+    !userId ||
+    !origin ||
+    !destination ||
+    !dateTime ||
+    !notificationType ||
+    !tripId
+  )
+    return;
 
-    const {formattedDate, formattedTime} = DateFormatter(dateTime);
+  const { formattedDate, formattedTime } = DateFormatter(dateTime);
 
-    // insert a new entry to the notification table
-    if (notificationType === "ride_requested") {
+  switch (notificationType) {
+    case "ride_requested": {
+      // retrieve drivers IDs and Expo push tokens
+      const { data: recipients, error: recipientsError } = await supabase
+        .from("profile")
+        .select("id, expo_push_token")
+        .contains("role", ["driver"])
+        .not("expo_push_token", "is", null);
 
-        const {error} = await supabase.from("notification").insert({
-            user_id: userId,
-            title: "A new ride request",
-            body: `From: ${origin.address}\nTo: ${destination.address}\nOn: ${formattedDate} at ${formattedTime}`,
-            type: "ride_requested",
-            trip_id: tripId,
-        })
+      console.log(recipients);
+
+      if (recipientsError) throw recipientsError;
+
+      // insert a new to the notification table
+      for (const recipient of recipients) {
+        const { error } = await supabase.from("notification").insert({
+          user_id: userId,
+          recipient_id: recipient.id,
+          recipient_token: recipient.expo_push_token,
+          title: "A new ride request",
+          body: `From: ${origin.address}\nTo: ${destination.address}\nOn: ${formattedDate} at ${formattedTime}`,
+          type: "ride_requested",
+          trip_id: tripId,
+        });
 
         if (error) throw error;
+      }
     }
+
+    default:
+      return;
+  }
 }
