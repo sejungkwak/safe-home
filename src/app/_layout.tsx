fded7e34480150d4a58874804336de2375216912
sejungkwak <kwak.sejung@gmail.com>
2026-04-18 19:57:31 +0100

    refactor: add Stack.Protected guard for session checks

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,5 @@
 import * as Notifications from "expo-notifications";
-import { Stack, useRouter, useSegments } from "expo-router";
-import { useEffect } from "react";
+import { Stack } from "expo-router";
 import { useColorScheme } from "react-native";
 import { MD3DarkTheme, MD3LightTheme, PaperProvider } from "react-native-paper";
 
@@ -12,31 +11,21 @@ import { TripProvider } from "@/context/trip";
 import { useRouterNotifications } from "@/hooks/use-router-notifications";
 import "../../global.css";
 
-// notification settings
+// enable the notification to show the alert while the app is running.
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
-    shouldPlaySound: true,
+    shouldPlaySound: true, // required for Android drop-down alert
     shouldSetBadge: true,
     shouldShowBanner: true,
     shouldShowList: true,
   }),
 });
 
+/**
+ * Sets up theme, notification routing,
+ * and wraps the app with context providers.
+ */
 export default function RootLayout() {
-  useRouterNotifications();
-  return (
-    <SessionProvider>
-      <InitialLayout />
-    </SessionProvider>
-  );
-}
-
-// https://supabase.com/blog/react-native-storage
-function InitialLayout() {
-  const { session, initialised } = useSession();
-  const segments = useSegments();
-  const router = useRouter();
-
   const colorScheme = useColorScheme();
 
   const paperTheme =
@@ -44,63 +33,62 @@ function InitialLayout() {
       ? { ...MD3DarkTheme, roundness: 2, colors: Colors.dark }
       : { ...MD3LightTheme, roundness: 2, colors: Colors.light };
 
-  useEffect(() => {
-    if (!initialised) return;
-
-    // check if the path/url is in the (tabs) group
-    const inTabsGroup = segments[0] === "(tabs)";
-
-    if (session && !inTabsGroup) {
-      // redirect authenticated users to the home screen
-      router.replace("/(tabs)/home");
-    } else if (!session) {
-      // redirect unauthenticated users to the sign in screen
-      router.replace("/");
-    }
-  }, [session, initialised]);
+  useRouterNotifications();
 
   return (
-    <PaperProvider theme={paperTheme}>
-      <RoleProvider>
-        <TripProvider>
-          <NotificationProvider>
-            <RootStack />
-          </NotificationProvider>
-        </TripProvider>
-      </RoleProvider>
-    </PaperProvider>
+    <SessionProvider>
+      <PaperProvider theme={paperTheme}>
+        <RoleProvider>
+          <TripProvider>
+            <NotificationProvider>
+              <RootStack />
+            </NotificationProvider>
+          </TripProvider>
+        </RoleProvider>
+      </PaperProvider>
+    </SessionProvider>
   );
 }
 
+/**
+ * Sets up the app navigation.
+ * Shows the sign in and sign up screens to unauthorised users,
+ * and tabs screens to authorised users.
+ */
 function RootStack() {
+  const { user, initialised } = useSession();
   return (
     <Stack>
-      <Stack.Screen
-        name="index"
-        options={{
-          headerTitle: "Sign In",
-          headerShadowVisible: false,
-          headerBackVisible: false,
-        }}
-      />
-      <Stack.Screen
-        name="sign-up"
-        options={{
-          headerTitle: "Sign Up",
-          headerShadowVisible: false,
-          headerBackVisible: false,
-        }}
-      />
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen
-        name="booking"
-        options={{
-          headerTitle: "Pickup Time",
-          headerShadowVisible: false,
-          headerBackVisible: true,
-          headerBackButtonDisplayMode: "minimal",
-        }}
-      />
+      <Stack.Protected guard={!initialised || !user}>
+        <Stack.Screen
+          name="index"
+          options={{
+            headerTitle: "Sign In",
+            headerShadowVisible: false,
+            headerBackVisible: false,
+          }}
+        />
+        <Stack.Screen
+          name="sign-up"
+          options={{
+            headerTitle: "Sign Up",
+            headerShadowVisible: false,
+            headerBackVisible: false,
+          }}
+        />
+      </Stack.Protected>
+      <Stack.Protected guard={!!initialised && !!user}>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen
+          name="booking"
+          options={{
+            headerTitle: "Pickup Time",
+            headerShadowVisible: false,
+            headerBackVisible: true,
+            headerBackButtonDisplayMode: "minimal",
+          }}
+        />
+      </Stack.Protected>
     </Stack>
   );
 }
